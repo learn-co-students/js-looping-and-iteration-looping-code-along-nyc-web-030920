da27922b2abf5c9b906212e7c33d083313b77da4
MisterC1ean <robert.keller22@gmail.com>
2020-04-19 16:24:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Code your solutions in this file
 
 function writeCards(names, event){
-    for ( i= 0; i < names.length; i++){
-        console.log(`Thank you, ${names[i]} for the wonderful ${event} gift!`)
+    let i;
+    let cards = [];
+    for (i= 0; i < names.length; i++){
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
     }
-
+    return cards;
 }
 function countDown(num){
     
