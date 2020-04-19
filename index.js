00687b92ba4d6cf24d420dc7df6f5b5cd143af75
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-04-19 17:26:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, string) {
+  let thankYouMessages = [];
+  for(let i =0; i < array.length; i++) {
+    thankYouMessages.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+  }
+  return thankYouMessages;
+}
+
+function countDown(num) {
+  while(num >= 0) {
+    console.log(num)
+    num--
+  }
+}
