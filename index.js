d25a54bfb70d41cdc5106e466f926b57e458492a
Yahyagok <ygok17@hotmail.com>
2020-04-20 10:02:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+
+function writeCards(names, word) {
+    let thankYouCards = [];
+    let i = 0;
+    while (i < names.length) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${word} gift!`);
+    i++;
+    }
+    return thankYouCards;
+    
+}
+
+function countDown(number) {
+
+    while (number > 0 ) {
+        console.log(number);
+        number -=1 
+    }
+    console.log( number );
+
+}
