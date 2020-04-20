4c901e2c2c6840e6d06b8a51da462dc68ed4823c
Lix311 <alex.sterrantino@gmail.com>
2020-04-19 20:54:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names,event){
+let g_array = []
+    for(let i = 0;i < names.length;i++){
+    g_array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+}
+return g_array 
+}
+
+function countDown(int){
+    while (int > -1) {
+        console.log(int)
+        int--
+    }
+}
