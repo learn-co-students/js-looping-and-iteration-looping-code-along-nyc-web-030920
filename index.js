59d400aff1524315e104494c1d8cdf89c5453a59
CBreakr <github email address>
2020-04-19 20:46:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names){
+    const cards = [];
+    for(let name of names){
+        cards.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+    return cards;
+}
+
+function countDown(start){
+    let innerCount = start;
+    while(innerCount >= 0){
+        console.log(innerCount);
+        innerCount--;
+    }
+}
