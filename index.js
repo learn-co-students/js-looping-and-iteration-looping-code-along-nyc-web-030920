da79fba9792da175efd891f3637b7864c39419ad
Yueying <wangyy395@gmail.com>
2020-04-20 09:58:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arr, msg) {
+    let ans = [];
+    for (let i = 0; i < arr.length; i++) {
+        ans.push(`Thank you, ${arr[i]}, for the wonderful ${msg} gift!`);
+    }
+    return ans;
+}
+
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
