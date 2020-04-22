5e9bba881078ff6591bb6e7496460bb1d92d1a3d
duruchima <duruchima1@gmail.com>
2020-04-22 19:14:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(friends) {
+    let array = []
+    for (let i = 0; i < friends.length; i++) {
+        array.push(`Thank you, ${friends[i]}, for the wonderful surprise gift!`);
+    }
+
+    return array;
+}
+
+function countDown(num){
+    let countdown = num
+    while (countdown > 0) {
+        console.log(countdown--)
+    }
+    if (countdown == 0) {console.log(countdown)}
+}
